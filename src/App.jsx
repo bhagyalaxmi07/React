1d680183a95a2aa6de3e9e1354ceb80ba4401967
bhagyalaxmi07 <159108114+bhagyalaxmi07@users.noreply.github.com>
2025-02-27 09:50:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [text, setText] = useState("");
+
+  return (
+      <div className="App">
+        <input
+          type="text"
+          placeholder="Type Something..."
+          value={text}
+          onChange={(e)=>setText(e.target.value)}
+          />
+          <h1><strong>You Typed:{text}</strong></h1>
+          <h1><strong>Length:{text.length}</strong></h1>
+      </div>
+  );
+}
+
+export default App
